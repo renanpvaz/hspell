134a66e798ef9f4320d9a8179be4d530ffcf88be
renanpvaz <renan.piresvz@gmail.com>
2019-06-22 19:47:34 -0300

    Add word count

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,14 @@
 module Main where
 
 import qualified Data.Set as Set
-
-words =
-    [ "something"
-    , "soothing"
-    ]
-
+import Data.Char
+import qualified Data.Map as Map
+import qualified Debug.Trace as Debug
+import qualified Data.List as List
+import System.IO
+import Data.List.Split (wordsBy)
 
 letters = ['a'..'z']
-
 
 known word =
   word `elem` [ "something"
@@ -40,11 +39,8 @@ inserts :: [(String, String)] -> [String]
 inserts =
   concatMap (\(l, r) -> map (\c -> l ++ c:r) letters)
 
-
-
 edits word =
-  let
-      splits = split 0 word
+  let splits = split 0 word
   in
   Set.fromList $
     deletes splits
@@ -52,7 +48,28 @@ edits word =
     ++ replaces splits
     ++ inserts splits
 
+toLowerString :: String -> String
+toLowerString = map toLower
+
+count :: Map.Map String Int -> String -> Map.Map String Int
+count counts word =
+  let loweredWord = toLowerString word
+  in
+  case Map.lookup loweredWord counts of
+    Just c -> Map.insert loweredWord (c + 1) counts
+    Nothing -> Map.insert loweredWord 1 counts
+
+wordCount =
+  foldl count Map.empty
+
+mostCommon =
+  take 10
+    . List.sortOn (negate . snd)
+    . Map.toList
 
 main :: IO ()
 main = do
-  putStrLn $ show (Set.filter known $ edits "somthing")
+  handle <- openFile "words.txt" ReadMode
+  contents <- hGetContents handle
+  putStr $ show $ mostCommon $ wordCount $ wordsBy (not . isLetter) contents
+  hClose handle
