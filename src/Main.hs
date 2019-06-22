9afd068af3c08c01f960d967c4fbefcf474e8785
renanpvaz <renan.piresvz@gmail.com>
2019-06-22 20:50:02 -0300

    Select correction candidates

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import qualified Data.Set                      as Set
 import           Data.Char
+import           Data.Maybe
 import qualified Data.Map                      as Map
 import qualified Debug.Trace                   as Debug
 import qualified Data.List                     as List
@@ -10,7 +11,7 @@ import           Data.List.Split                ( wordsBy )
 
 letters = ['a' .. 'z']
 
-known word = word `elem` ["something", "soothing"]
+known counts = filter (\w -> Map.member w counts)
 
 split n word
   | n <= length word = (take n word, drop n word) : split (n + 1) word
@@ -54,9 +55,26 @@ wordCount = foldl count Map.empty
 
 mostCommon = take 10 . List.sortOn (negate . snd) . Map.toList
 
+freq :: String -> Map.Map String Int -> Int
+freq word counts =
+  fromMaybe 0 $ Map.lookup word counts
+
+probability :: Fractional a => String -> Map.Map String Int -> a
+probability word counts =
+  let n = fromIntegral $ sum $ Map.elems counts
+      f = fromIntegral $ (freq word counts)
+  in f / n
+
+
+candidates word counts =
+  known counts [word] ++ known counts (Set.toList $ edits word)
+
 main :: IO ()
 main = do
   handle   <- openFile "words.txt" ReadMode
   contents <- hGetContents handle
-  putStr $ show $ mostCommon $ wordCount $ wordsBy (not . isLetter) contents
+  input <- getLine
+  let counts = wordCount $ wordsBy (not . isLetter) contents
+  putStr $ show $ candidates input counts
+  -- putStr $ show $ mostCommon $ wordCount $ wordsBy (not . isLetter) contents
   hClose handle
