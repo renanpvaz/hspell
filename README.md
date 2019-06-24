bf65d8bf662b55952288fca1e92e41f55ef018a8
renanpvaz <renan.piresvz@gmail.com>
2019-06-24 10:16:21 -0300

    Use reader for word count

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# spelling-corrector
+# hspell
+
+Haskell implementation of a simple spelling corrector based on https://norvig.com/spell-correct.html.
--- src/Main.hs
+++ src/Main.hs
@@ -9,17 +9,20 @@ import qualified Data.List                     as List
 import           System.IO
 import           Data.List.Split                ( wordsBy )
 import           Control.Monad
+import           Control.Monad.Reader
 
 type WordCount = Map.Map String Int
+
+rightNotNull = filter ((/=) "" . snd)
+
+withLetters f = concatMap (\split' -> map (f split') ['a' .. 'z'])
+
+words' = wordsBy (not . isLetter)
 
 split :: Int -> String -> [(String, String)]
 split n word
   | n <= length word = (take n word, drop n word) : split (n + 1) word
   | otherwise        = []
-
-rightNotNull = filter ((/=) "" . snd)
-
-withLetters f = concatMap (\split' -> map (f split') ['a' .. 'z'])
 
 deletes :: [(String, String)] -> [String]
 deletes = map (\(l, _ : r) -> l ++ r) . rightNotNull
@@ -46,44 +49,49 @@ edits word =
 lower :: String -> String
 lower = map toLower
 
-count :: Map.Map String Int -> String -> Map.Map String Int
-count counts word =
+countOne :: WordCount -> String -> WordCount
+countOne counts word =
   let word' = lower word
   in  case Map.lookup word' counts of
         Just c  -> Map.insert word' (c + 1) counts
         Nothing -> Map.insert word' 1 counts
 
 wordCount :: String -> WordCount
-wordCount = foldl count Map.empty . wordsBy (not . isLetter)
+wordCount = foldl countOne Map.empty . words'
 
-freq :: String -> Map.Map String Int -> Int
-freq word counts = fromMaybe 0 $ Map.lookup word counts
+freq :: WordCount -> String -> Int
+freq count word = fromMaybe 0 $ Map.lookup word count
 
 probability :: Fractional a => WordCount -> String -> a
-probability counts word =
-  let n = fromIntegral $ sum $ Map.elems counts
-      f = fromIntegral $ freq word counts
+probability count word =
+  let n = fromIntegral $ sum $ Map.elems count
+      f = fromIntegral $ freq count word
   in  f / n
 
-known :: WordCount -> [String] -> [String]
-known counts = filter (`Map.member` counts)
+known :: [String] -> Reader WordCount [String]
+known words = do
+  count <- ask
+  return $ filter (`Map.member` count) words
 
-candidates :: WordCount -> String -> [String]
-candidates counts word =
-  known counts [word] ++ known counts (Set.toList $ edits word)
+candidates :: String -> Reader WordCount [String]
+candidates word = do
+  a <- known [word]
+  b <- known (Set.toList $ edits word)
+  case a of
+    [] -> return b
+    _  -> return a
 
-correction :: WordCount -> String -> String
-correction counts =
-  head . List.sortOn (negate . probability counts) . candidates counts
-
-correct c =
-  putStr . show . correction c
+correction :: String -> Reader WordCount String
+correction word = do
+  count <- ask
+  c     <- candidates word
+  return $ head $ List.sortOn (negate . probability count) c
 
 main :: IO ()
 main = do
-  handle <- openFile "words.txt" ReadMode
+  handle   <- openFile "words.txt" ReadMode
   contents <- hGetContents handle
-  let counts = wordCount contents
-  input <- getLine
-  correct counts input
+  input    <- getLine
+  putStr $ show $ map (\w -> runReader (correction w) (wordCount contents))
+                      (words' input)
   hClose handle
